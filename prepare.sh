64d98758f5ed6b82f2ba1b24a3e3f1fcc31926fa
sakana7392 <k_hiro@kzd.biglobe.ne.jp>
2022-12-31 07:50:49 +0900

    githubのURLを修正

--- prepare.sh
+++ prepare.sh
@@ -13,10 +13,10 @@ echo "# $1" >$1/README.md
 echo "## 概要" >>$1/README.md
 echo "## 実装の詳細" >>$1/README.md
 echo "## 実装するメリットとデメリット" >>$1/README.md
-echo "## メリット" >>$1/README.md
-echo "## デメリット" >>$1/README.md
+echo "### メリット" >>$1/README.md
+echo "### デメリット" >>$1/README.md
 echo "## 実装したコードへのリンク" >>$1/README.md
 for i in "${language[@]}"
 do
-    echo "* [${i}](https://github.com/sakana7392/Design_Patterns/$1/${i})" >>$1/README.md
+    echo "* [${i}](https://github.com/sakana7392/Design_Patterns/tree/main/$1/${i})" >>$1/README.md
 done
