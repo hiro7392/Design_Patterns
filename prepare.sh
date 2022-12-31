e368a433ebca1ec50a7644dc298ebf658689105b
sakana7392 <k_hiro@kzd.biglobe.ne.jp>
2022-12-31 16:32:02 +0900

    Goの初期化を修正

--- prepare.sh
+++ prepare.sh
@@ -6,6 +6,14 @@ language=("c++" "TypeScript" "Go")
 for i in "${language[@]}"
 do
     mkdir $1/${i}
+    if [ ${i}  == "Go" ]
+        then
+        #Goについてはgo mod tidyをしておく
+        cd $1/${i}
+        go mod init github.com/sakana7392/Design_Patterns/tree/main/$1/${i}
+        go mod tidy
+        cd ../..
+    fi
 done
 # パターンの概要をまとめるREADME
 touch $1/README.md
@@ -15,7 +23,7 @@ echo "## 実装の詳細" >>$1/README.md
 echo "## 実装するメリットとデメリット" >>$1/README.md
 echo "### メリット" >>$1/README.md
 echo "### デメリット" >>$1/README.md
-echo "## 実装したコードへのリンク" >>$1/README.md
+echo "## 実装したコード" >>$1/README.md
 for i in "${language[@]}"
 do
     echo "* [${i}](https://github.com/sakana7392/Design_Patterns/tree/main/$1/${i})" >>$1/README.md
