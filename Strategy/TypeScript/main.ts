3a8745fd9836e884e629bfbab6b5bca67aa45db5
sakana7392 <k_hiro@kzd.biglobe.ne.jp>
2023-01-01 13:50:20 +0900

    Tsでアルゴ部分は簡易的に実装

--- Strategy/TypeScript/main.ts
+++ Strategy/TypeScript/main.ts
@@ -1,11 +1,22 @@
+function getRandomInt(max) {
+    return Math.floor(Math.random() * max);
+  }
+  
 class Player{
     strategy:Strategy;
-
+    results:number[][]=[
+        [0,0,0],
+        [0,0,0],
+        [0,0,0],
+    ];
+    constructor(strategy:Strategy){
+        this.strategy=strategy;
+    }
 }
 
 interface Strategy{
     nextHand():number;
-    study():void;
+    study(boolean):void;
 };
 enum Hand {
     Rock, // 0
@@ -13,32 +24,79 @@ enum Hand {
     Papar, // 2
 }
 enum Result {
-    Victory, // 0
+    Win, // 0
     Lose, // 1
-    Draw, // 2
+    Even, // 2
 }
+const Hands:string[]=["Rock","SESSORS","PAPER"];
 
 class WinningStrategy implements Strategy{
     private beforeHand:number;
     private nowHand:number;
-    nextHand(): number {
-        return 0
+    private won:boolean=false;
+    constructor(){
+        this.beforeHand=0;
+        this.nowHand=0;
     }
-    study(): void {
-        
+    //  前回勝っていたら同じ手、そうでなければ違う手を出す
+    public nextHand(): number {
+        if(!this.won){
+            this.beforeHand=getRandomInt(2);
+        }
+        return this.beforeHand;
+    }
+    public study(result:boolean): void {
+        this.won=result;
+
     }
 };
 
-class ProbStrategy implements Strategy{
-
-    nextHand():number{
-        return 0;
+class RandomStrategy implements Strategy{
+    private beforeHand:number;
+    public nextHand():number{
+        this.beforeHand=getRandomInt(3)
+        return this.beforeHand;
     }
-    study(): void {
+    public study(result:boolean): void {
         
     }
-}   
+} 
+function Battle(Player1:Player,Player2:Player):number{
+    let result=0;
+    const p1:number=Player1.strategy.nextHand();
+    const p2:number=Player2.strategy.nextHand();
+    console.log(`${Hands[p1]} vs ${Hands[p2]} `);
+
+    if((p1+1)%3===p2){
+        console.log("player1 won!");
+        result=0;
+    }else if((p2+1)%3===p1){
+        console.log("player2 won!");
+        result=1;
+    }else{
+        console.log("DRAW!");
+        result=2;
+    }
+    Player1.strategy.study((p1+1)%3===p2);
+    Player2.strategy.study((p2+1)%3===p1);
+    return result;
+}
 
 function clientCode(){
-    
+    const Player1=new Player(new WinningStrategy());
+    const Player2=new Player(new RandomStrategy());
+
+    let win:number=0,lose:number=0,draw:number=0;
+    for(let i=0;i<100;i++){
+        const res=Battle(Player1,Player2)
+        if(res===0){
+            win++;
+        }else if(res===1){
+            lose++;
+        }else{
+            draw++;
+        }
+    }
+    console.log(`player1 result\n win:${win} lose:${lose} draw:${draw}`);
 }
+clientCode()
